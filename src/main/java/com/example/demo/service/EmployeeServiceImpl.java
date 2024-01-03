4eae1252e4618a8ec7a6ab88e27f8338b3021fd3
aki0000 <akihisa.fromm1@gmail.com>
2024-01-04 04:39:32 +0900

    Login process created

---
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.selectAll();
+    }
+
+    @Override
+    public Employee findById(String employeeId) {
+        return employeeRepository.selectByEmployeeId(employeeId);
+    }
+}
