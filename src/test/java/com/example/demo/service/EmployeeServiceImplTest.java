f581e88c145135458df4fd7d23ce9fa3a96d4736
aki0000 <akihisa.fromm1@gmail.com>
2024-01-05 14:03:04 +0900

    created service for IT

--- src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
+++ src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.exceptions.base.MockitoException;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.demo.entity.Employee;
